a928a54cfa54163ef3e87b0a454c2c5a4a797d81
Yashwanth <yashwanth@qugates.in>
2025-09-20 11:52:29 +0530

    refator : modified video model

--- backend/models/videos.js
+++ backend/models/videos.js
@@ -1,27 +1,56 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
 
-const Videos = sequelize.define('videos', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+const Videos = sequelize.define(
+  "videos",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    uploderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    discription: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    uplodedTime: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    videoUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    thumbnailUrl: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    containerId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isPublic: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+    views: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
-  uploderId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-}, {
-  timestamps: true, // createdAt, updatedAt
-});
+  {
+    timestamps: true, // createdAt, updatedAt
+  }
+);
 
 module.exports = Videos;
